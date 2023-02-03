53d67de81f1294ad34b585938d42711fb84eb507
10200136 <wwq@fef.com>
2023-02-03 11:45:57 +0300

    db output

--- views/site/index.php
+++ views/site/index.php
@@ -2,11 +2,30 @@
 
 /** @var yii\web\View $this */
 
+use app\models\Category;
+use app\models\User;
+
 $this->title = 'My Yii Application';
+$problems = \app\models\Problem::find()->indexBy('id')->all();
 ?>
 <div class="site-index">
 
     <div class="body-content">
-        <p>dqwqwd</p>
+        <div class="d-flex flex-row gap-5 flex-lg-wrap">
+            <?php
+            foreach ($problems as $problem) { ?>
+                <div class="problem_container">
+                    <p><?=$problem['id']?></p>
+                    <p><?=$problem['name']?></p>
+                    <p><?=$problem['description']?></p>
+                    <p><?= User::findIdentity($problem['id'])->fio?></p>as
+                    <p><?= Category::find()->where(['id' => $problem['category_id']])->one()->title?></p>
+                    <p><?=$problem['status']?></p>
+                    <p><?=$problem['photo_before']?></p>
+                    <p><?=$problem['photo_after']?></p>
+                </div>
+            <?php } ?>
+        </div>
     </div>
+
 </div>
